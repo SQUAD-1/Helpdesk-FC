cf27edf38be9a294bda043fda1ac521dbf9e7c55
Cleyton Lucas Cabral da Silva <Cleytonlucasiurd@gmail.com>
2022-10-12 23:03:28 -0300

    Algumas adições (Novas Pastas, retiradas de elementos desnecessários))

---
+++ central-atendimento/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        
+        <h1>
+          Tela Inicial
+        </h1>
+        <p>O que iremos colocar aqui?</p>
+        
+    </div>
+  );
+}
+
+export default App;
