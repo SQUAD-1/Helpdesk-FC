f0342cab489adf7a3ac88f7ec3dcda0a963f6d38
CleytonLu <cleytonlucasiurd@gmail.com>
2022-10-31 20:59:21 -0300

    Finalização da parte endereço da página inicial da tela principal de atendente, e alguns ajustes

--- central-atendente/src/Tamplates/Atendente/Principal/Dados/Dados.jsx
+++ central-atendente/src/Tamplates/Atendente/Principal/Dados/Dados.jsx
@@ -1,4 +1,5 @@
 import { Contato } from "./Contato/Contato"
+import { Endereco } from "./Endereco/Endereco"
 import { Informacoes } from "./Informacoes/Informacoes"
 
 export const Dados = () => {
@@ -6,8 +7,10 @@ export const Dados = () => {
         <>
 
             <Informacoes nome="Luiz Mota" nomeCompleto="Luiz Henrique Mota" dataNasciemento="24/11/1978" cpf="081.340.051-01" rg="513.431-02" />
-            <Contato tel="Não disponibilizado" cel="(81) 98452 - 0004" whats="Sim" email="luizmottak@fcx.com.br" />
             
+            <Contato tel="Não disponibilizado" cel="(81) 98452 - 0004" whats="Sim" email="luizmottak@fcx.com.br" />
+
+            <Endereco endereco="Rua dos Amores" numero="952" complemento="Sem Complemento" referencia="Próximo ao Chão" cidade="Recife" estado="Pernambuco" cep="05204-533" />
 
         </>
     )
