d23e895d2c72a18592a23970590298a1aac354b2
CleytonLu <cleytonlucasiurd@gmail.com>
2022-10-31 20:02:53 -0300

    Parte de Contato da Tela principal do atendente finalizada

--- central-atendente/src/Tamplates/Atendente/Principal/Dados/Dados.jsx
+++ central-atendente/src/Tamplates/Atendente/Principal/Dados/Dados.jsx
@@ -4,8 +4,11 @@ import { Informacoes } from "./Informacoes/Informacoes"
 export const Dados = () => {
     return(
         <>
+
             <Informacoes nome="Luiz Mota" nomeCompleto="Luiz Henrique Mota" dataNasciemento="24/11/1978" cpf="081.340.051-01" rg="513.431-02" />
             <Contato tel="Não disponibilizado" cel="(81) 98452 - 0004" whats="Sim" email="luizmottak@fcx.com.br" />
+            
+
         </>
     )
 }
