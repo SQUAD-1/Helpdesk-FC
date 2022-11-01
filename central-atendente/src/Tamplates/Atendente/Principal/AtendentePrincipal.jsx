77d73201055e8f64dde289d5b3cc421086d27c98
CleytonLu <cleytonlucasiurd@gmail.com>
2022-11-01 18:44:06 -0300

    Finalização da tela princpal

--- central-atendente/src/Tamplates/Atendente/Principal/AtendentePrincipal.jsx
+++ central-atendente/src/Tamplates/Atendente/Principal/AtendentePrincipal.jsx
@@ -1,11 +1,14 @@
 import { BarraLateral } from "./BarraLateral/BarraPrincipal"
 import { Dados } from "./Dados/Dados"
+import { Estatisticas } from "./Estatisticas/Estatisticas"
 
 export const AtendentePrincipal = () => {
     return(
         <>
             <BarraLateral />
             <Dados />
+
+            <Estatisticas />
         </>
     )
 }
---
+++ central-atendente/src/Tamplates/Atendente/Principal/Estatisticas/Estatisticas.jsx
@@ -0,0 +1,37 @@
+import './Estatisticas.css'
+
+export const Estatisticas = () => {
+    return(
+        <section>
+            <h1 className='title'>Estatísiticas</h1>
+
+            <div className='container'>
+                <div className='comprasFeitas'>
+                    <h2>Nº de compras feitas</h2>
+                    <h2>01 compras feitas</h2>
+                </div>
+
+                <div className='adquiridos'>
+                    <h2>Produtos Adquiridos</h2>
+                    <h2>03 Produtos</h2>
+                </div>
+
+                <div className='problemas'>
+                    <h2>Problemas Reportados</h2>
+                    <h2>02 Problemas</h2>
+                </div>
+
+                <div>
+                    <h2>Ligações Realizadas</h2>
+                    <h2> 02 Ligações</h2>
+                </div>
+
+                <div className='ligacao'>
+                    <h2>Última Ligação</h2>
+                    <h2>18/09/22 às 10:39</h2> {/*Automação Futura*/}
+                </div>
+            </div>
+
+        </section>
+    )
+}
