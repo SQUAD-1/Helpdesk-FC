17b518bafb0d13d0c767332151699a5732986355
CleytonLu <cleytonlucasiurd@gmail.com>
2022-11-02 22:32:02 -0300

    Alteração no Component da Barra Lateral

--- central-atendente/src/Components/BarraLateral/BarraPrincipal.jsx
+++ central-atendente/src/Components/BarraLateral/BarraPrincipal.jsx
@@ -1,12 +1,12 @@
 //import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './BarraPrincipal.css'
 
-import logo from './Img/ferreiracosta.svg'
-import Bullet from './Img/Bullet.svg'
-import Rectangle from './Img/Rectangle.svg'
-import Rectangle2 from './Img/Rectangle2.svg'
-import Rectangle3 from './Img/Rectangle3.svg'
-import Rectangle4 from './Img/Rectangle4.svg'
+import logo from './Img/logo.svg'
+import inicio from './Img/inicio.svg'
+import Atendimentos from './Img/Atendimentos.svg'
+import Chamados from './Img/Chamados.svg'
+import Consulta from './Img/Consulta.svg'
+import Setores from './Img/Consulta.svg'
 
 export const BarraLateral = () => {
     return(
@@ -18,23 +18,23 @@ export const BarraLateral = () => {
             
                 <article className='iconRedirection'>
                     <div className='Bullet'>
-                        <img src={Bullet} alt="Bullet" />
+                        <img src={inicio} alt="inicio" />
                     </div>
 
                     <div className='Bullet2'>
-                        <img src={Rectangle} alt="Rectangle" />    
+                        <img src={Atendimentos} alt="Atendimentos" />    
                     </div>
 
                     <div className='Bullet3'>
-                        <img src={Rectangle2} alt="Rectangle2" />
+                        <img src={Chamados} alt="Chamados" />
                     </div>
                     
                     <div className='Bullet4'>
-                        <img src={Rectangle3} alt="Rectangle3" />                        
+                        <img src={Consulta} alt="Consulta" />                        
                     </div>
 
                     <div className='Bullet4'>
-                        <img src={Rectangle4} alt="Rectangle4" />                        
+                        <img src={Setores} alt="Setores" />                        
                     </div>
 
                 </article>
